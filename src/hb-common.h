c2bc489aa82b6e683092f9d8cada4ea810fee89c
Tor Andersson <tor.andersson@artifex.com>
2018-04-12 16:45:13 +0200

    Restore support for Visual Studio 2005.
    * Revert "Remove some really old cruft".
    * Add a simple macro for round().
    * Guard use of BitScan intrinsics with appropriate ifdefs.

--- src/hb-common.h
+++ src/hb-common.h
@@ -43,14 +43,33 @@
 # endif /* !__cplusplus */
 #endif
 
+#if !defined (HB_DONT_DEFINE_STDINT)
+
 #if defined (_SVR4) || defined (SVR4) || defined (__OpenBSD__) || \
     defined (_sgi) || defined (__sun) || defined (sun) || \
     defined (__digital__) || defined (__HP_cc)
 #  include <inttypes.h>
 #elif defined (_AIX)
 #  include <sys/inttypes.h>
+/* VS 2010 (_MSC_VER 1600) has stdint.h */
+#elif defined (_MSC_VER) && _MSC_VER < 1600
+typedef __int8 int8_t;
+typedef unsigned __int8 uint8_t;
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
 #else
 #  include <stdint.h>
+#endif
+
+#endif
+
+/* VS 2013 (_MSC_VER 1800) has round() */
+#if defined(_MSC_VER) && _MSC_VER < 1800
+#define round(X) (floor((X)+0.5))
 #endif
 
 HB_BEGIN_DECLS
