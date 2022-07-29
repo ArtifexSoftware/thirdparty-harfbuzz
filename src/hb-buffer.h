2df8b1c6e003d9eb74b13ce6397855001a337384
Behdad Esfahbod <behdad@behdad.org>
2022-07-29 13:55:39 -0600

    Prototype glyph flag safe-to-kashida
    Fixes https://github.com/harfbuzz/harfbuzz/issues/3721

--- src/hb-buffer.h
+++ src/hb-buffer.h
@@ -151,8 +151,9 @@ typedef struct hb_glyph_info_t {
 typedef enum { /*< flags >*/
   HB_GLYPH_FLAG_UNSAFE_TO_BREAK		= 0x00000001,
   HB_GLYPH_FLAG_UNSAFE_TO_CONCAT	= 0x00000002,
+  HB_GLYPH_FLAG_SAFE_TO_KASHIDA		= 0x00000004,
 
-  HB_GLYPH_FLAG_DEFINED			= 0x00000003 /* OR of all defined flags */
+  HB_GLYPH_FLAG_DEFINED			= 0x00000007 /* OR of all defined flags */
 } hb_glyph_flags_t;
 
 HB_EXTERN hb_glyph_flags_t
